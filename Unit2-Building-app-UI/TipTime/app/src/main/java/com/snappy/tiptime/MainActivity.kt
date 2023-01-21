5878a18594d4a660c65e0752ad190d6306415c66
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-21 23:39:36 +0530

    Set Keyboard Action & tip-percent Done.

--- Unit2-Building-app-UI/TipTime/app/src/main/java/com/snappy/tiptime/MainActivity.kt
+++ Unit2-Building-app-UI/TipTime/app/src/main/java/com/snappy/tiptime/MainActivity.kt
@@ -4,18 +4,21 @@ import android.icu.text.NumberFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,14 +40,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit){
+fun EditNumberField(
+    value: String,
+    @StringRes label: Int,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions
+){
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = {Text( stringResource(R.string.cost_of_service))},
+        label = {Text( stringResource(label))},
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
     )
 
 }
@@ -55,18 +65,48 @@ fun TipTimeScreen(){
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         var amountInput by remember { mutableStateOf("0")}
+        var tipInput by remember { mutableStateOf("15.0")}
         val amount = amountInput.toDoubleOrNull()?:0.0
-        val tip = calculateTip(amount)
+        val tipPercent  = tipInput.toDoubleOrNull()?:0.0
+        val tip = calculateTip(amount, tipPercent)
+        val focusManager = LocalFocusManager.current
 
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally
+            modifier = Modifier.align(
+                CenterHorizontally
             )
         )
         Spacer(Modifier.height(16.dp))
 
-        EditNumberField(amountInput, onValueChange = {amountInput = it})
+        EditNumberField(
+            amountInput,
+            R.string.bill_amount,
+            onValueChange =  { amountInput = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {focusManager.moveFocus(FocusDirection.Down)}
+            )
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        EditNumberField(
+            value = tipInput,
+            label = R.string.how_was_the_service,
+            onValueChange =  {tipInput = it},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }
+            )
+        )
 
         Spacer(modifier = Modifier.height(24.dp))
         Text(
